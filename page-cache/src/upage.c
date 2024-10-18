ac033ed922553400b4781217488ec8f74effc494
Richard <yongxiang080481@gmail.com>
2024-10-18 19:49:18 +0800

    test the init_page

---
+++ page-cache/src/upage.c
@@ -0,0 +1,16 @@
+#include <upage.h>
+#include <error.h>
+int init_page_cache(void)
+{
+    char* page_cache = spdk_malloc();
+    if(init_ssd_cache())
+    {
+        perror("Error init ssd cache\n");
+    }
+}
+
+int main()
+{
+    init_page_cache();
+    return 0;
+}
