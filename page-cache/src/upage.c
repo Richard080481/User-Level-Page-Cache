4b45a5907c3e579f4792d8ce8c8ad8a4e8cf4e6a
hsu24 <bonnie.shiu.1@gmail.com>
2024-10-22 11:36:07 -0700

    implement init_page_cache and exit_page_cache

--- page-cache/src/upage.c
+++ page-cache/src/upage.c
@@ -1,16 +1,44 @@
 #include "upage.h"
+#include <spdk/env.h>
+#include <spdk/memory.h>
+#include <string.h>
 #include <error.h>
+
+struct page *page_head = NULL;
+
 int init_page_cache(void)
 {
-    char* page_cache = spdk_malloc();
     if(init_ssd_cache())
     {
         perror("Error init ssd cache\n");
+	return 1;
     }
+
+    page_head = (struct page*)spdk_zmalloc(PAGE_SIZE * 100, 0x1000, NULL, SPDK_ENV_NUMA_ID_ANY, SPDK_MALLOC_SHARE);
+
+    if(!page_head)
+    {
+	perror("Error: alloc_dma_buffer failed\n");
+	return 1;
+    }
+
+    return 0;
+
 }
 
-int main()
+int exit_page_cache(void)
+{
+    spdk_free(page_head);
+
+    exit_ssd_cache();
+
+    return 0;
+}
+
+
+int main(void)
 {
     init_page_cache();
+    exit_page_cache();
     return 0;
 }
