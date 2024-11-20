819a19da4f46e3cf013df6fb314c6e757f855a76
Richard <yongxiang080481@gmai.com>
2024-11-20 20:22:21 +0800

    remove pio function

--- page-cache/src/upage.c
+++ page-cache/src/upage.c
@@ -60,24 +60,6 @@ int exit_page_cache(void)
     return 0;
 }
 
-void write_pio(page* p)
-{
-    void* page_data_addr = ((char*)PHYS_BASE) + ((p - mem_map) * PAGE_SIZE);
-    operate operation = WRITE;
-    struct pio* head = create_pio(p->path_name, 0, p->index, operation, page_data_addr, 1); // submit pio to write the page into ssd, but page index is not set yet
-    submit_pio(head);
-    free_pio(head);
-}
-
-void read_pio(page* p)
-{
-    void* page_data_addr = ((char*)PHYS_BASE) + ((p - mem_map) * PAGE_SIZE);
-    operate operation = READ;
-    struct pio* head = create_pio(p->path_name, 0, p->index, operation, page_data_addr, 1); // submit pio to write the page into ssd, but page index is not set yet
-    submit_pio(head);
-    free_pio(head);
-}
-
 page* alloc_page(void)
 {
 
