12710ee89c0fbf1968b506448ac671baecce77c7
hsu24 <bonnie.shiu.1@gmail.com>
2024-11-28 17:49:12 +0800

    add functions of lru

--- page-cache/src/lru.h
+++ page-cache/src/lru.h
@@ -7,5 +7,17 @@
  * @brief Move a page to the head of the LRU list
  * @return No return value
  */
-void add_to_lru_head(page* page);
+void add_to_lru_head(lru_cache* lru_list, page* page);
+
+/**
+ * @brief Move a page which has already in the lru list to the head of the LRU list
+ * @return No return value
+ */
+void move_to_lru_head(lru_cache* lru_list, page* page);
+
+/**
+ * @brief Remove a page which is in the lru list
+ * @return No return value
+ */
+void remove_from_lru(lru_cache* lru_list, page* page);
 #endif
