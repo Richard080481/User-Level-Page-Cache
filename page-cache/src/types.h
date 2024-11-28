e1e879aff60537632cd6e25103398420785a1995
hsu24 <bonnie.shiu.1@gmail.com>
2024-11-28 17:49:28 +0800

    add lru entry

--- page-cache/src/types.h
+++ page-cache/src/types.h
@@ -5,6 +5,8 @@ typedef struct Header
     unsigned int PAGES;         // How many pages in the file header.
 } header;
 
+typedef struct lru_entry lru_entry;
+
 typedef struct PAGE
 {
     unsigned flag;
@@ -12,7 +14,22 @@ typedef struct PAGE
     unsigned int index;
     struct PAGE* prev;
     struct PAGE* next;
+    lru_entry* lru_ptr;
 }page;
+
+typedef struct lru_entry
+{
+    page* page_ptr;
+    struct lru_entry* prev;
+    struct lru_entry* next;
+} lru_entry;
+
+typedef struct lru_cache
+{
+    lru_entry* head;
+    lru_entry* tail;
+    int nr_pages;
+} lru_cache;
 
 enum pageflags
 {
@@ -21,16 +38,16 @@ enum pageflags
     PG_lru = 0x04,
 };
 
-typedef struct lru_cache
-{
-    page* head;
-    page* tail;
-    int nr_pages;
-} lru_cache;
-
 typedef struct page_free_list
 {
     page* head;
     int nr_free;
 } page_free_list;
+
+typedef struct hash_entry
+{
+    char* path_name;
+    page* page_ptr;
+    struct hash_entry* next;
+} hash_entry;
 #endif
