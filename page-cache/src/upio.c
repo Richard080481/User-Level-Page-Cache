e0d9ebb3072110bd3e287eca286d8e63541dd29b
hsu24 <bonnie.shiu.1@gmail.com>
2024-12-04 03:38:30 -0800

    fix the bug

--- page-cache/src/upio.c
+++ page-cache/src/upio.c
@@ -16,7 +16,7 @@ void write_pio(page* p, void* PHYS_BASE, page* mem_map)
 
     /* implement pio append */
     page* next_page = p->next;
-    for (int i = 1; i < page_cnt; i++)
+    for (unsigned int i = 1; i < page_cnt; i++)
     {
         page_data_addr = ((char*)PHYS_BASE) + ((next_page - mem_map) * PAGE_SIZE);
         append_pio(head, page_data_addr);
@@ -27,12 +27,12 @@ void write_pio(page* p, void* PHYS_BASE, page* mem_map)
     free_pio(head);
 }
 
-void read_pio(page* p, void* PHYS_BASE, page* mem_map)
+void read_pio(page* pg, void* PHYS_BASE, page* mem_map)
 {
     /* read the first page from the dm-cache to determine the total number of pages for the given path */
-    void* page_data_addr = ((char*)PHYS_BASE) + ((p - mem_map) * PAGE_SIZE);
+    void* page_data_addr = ((char*)PHYS_BASE) + ((pg - mem_map) * PAGE_SIZE);
     operate operation = READ;
-    struct pio* head = create_pio(p->path_name, 0, p->index, operation, page_data_addr, 1);
+    struct pio* head = create_pio(pg->path_name, 0, pg->index, operation, page_data_addr, 1);
     submit_pio(head);
     free_pio(head);
 
@@ -41,20 +41,32 @@ void read_pio(page* p, void* PHYS_BASE, page* mem_map)
     memcpy(&page_cnt, page_data_addr, PAGE_HEADER_SIZE);
 
     /* creat pio head (head is the second page of the path) */
-    operate operation = READ;
     page_cnt--; // the first page has already been read
 
-    page* next_page = p->next;
+    /* set the data of new page  */
+    page* next_page = alloc_page();
+
+    pg->next = next_page; // set first page's next
+    next_page->flag |= PG_lru;
+    next_page->index = 1;
+    next_page->next = NULL;
+
     page_data_addr = ((char*)PHYS_BASE) + ((next_page - mem_map) * PAGE_SIZE);
-    struct pio* head = create_pio(next_page->path_name, 0, next_page->index, operation, page_data_addr, page_cnt);
+    head = create_pio(next_page->path_name, 0, next_page->index, operation, page_data_addr, page_cnt);
 
     /* implement pio append */
-    next_page = next_page->next;
-    for (int i = 1; i < page_cnt; i++)
+    for (unsigned int i = 1; i < page_cnt; i++)
     {
-        page_data_addr = ((char*)PHYS_BASE) + ((next_page - mem_map) * PAGE_SIZE);
+        /* set the data of new page */
+        page* new_page = alloc_page();
+        next_page->next = new_page;
+        new_page->flag |= PG_lru;
+        new_page->index = i+1;
+        new_page->next = NULL;
+
+        page_data_addr = ((char*)PHYS_BASE) + ((new_page - mem_map) * PAGE_SIZE);
         append_pio(head, page_data_addr);
-        next_page = next_page->next;
+        next_page = new_page;
     }
     submit_pio(head);
     free_pio(head);
