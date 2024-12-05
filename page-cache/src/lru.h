398f7ec1feedc22f569106fb80f75f96fbf50a94
Richard <yongxiang080481@gmai.com>
2024-12-05 18:50:32 +0800

    update the comment of lru.h

--- page-cache/src/lru.h
+++ page-cache/src/lru.h
@@ -4,50 +4,95 @@
 #include "umalloc.h"
 #include "utypes.h"
 
-#define CACHE_SIZE 1000
+#define CACHE_SIZE 1000  // Define the size of the cache (hash table size)
 
-extern hash_entry* hash_table[CACHE_SIZE];
+extern hash_entry* hash_table[CACHE_SIZE];  // Global hash table to store the cache entries
 
 /**
- * @brief Move a page to the head of the LRU list
- * @return No return value
+ * @brief add a new page to the head of the LRU (Least Recently Used) list.
+ *
+ * This function moves the specified page to the head of the LRU list,
+ * making it the most recently used.
+ *
+ * @param lru_list The LRU cache list.
+ * @param pg The page to be moved to the head of the LRU list.
+ *
+ * @return No return value.
  */
 void add_to_lru_head(lru_cache* lru_list, page* pg);
 
 /**
- * @brief Move a page which has already in the lru list to the head of the LRU list
- * @return No return value
+ * @brief Move a page already in the LRU list to the head of the LRU list.
+ *
+ * If a page is already in the LRU list and has been accessed, it needs
+ * to be moved to the head to mark it as recently used.
+ *
+ * @param lru_list The LRU cache list.
+ * @param hd The entry in the LRU list that should be moved to the head.
+ *
+ * @return No return value.
  */
 void move_to_lru_head(lru_cache* lru_list, lru_entry* hd);
 
 /**
- * @brief Remove a page which is in the lru list
- * @return No return value
+ * @brief Remove a page from the LRU list.
+ *
+ * This function removes a specified page from the LRU list, making it
+ * no longer part of the cache.
+ *
+ * @param lru_list The LRU cache list.
+ * @param hd The entry to be removed from the LRU list.
+ *
+ * @return 0 if the removal is successful, non-zero if it fails.
  */
 int remove_from_lru(lru_cache* lru_list, lru_entry* hd);
 
 /**
- * @brief Count the hash value of the given path name
- * @return Hash value of the path name
+ * @brief Count the hash value for the given path name.
+ *
+ * This function computes the hash value for a given path name, which
+ * is used to locate the corresponding entry in the hash table.
+ *
+ * @param path_name The path name to compute the hash value for.
+ *
+ * @return The computed hash value for the path name.
  */
 unsigned int hash_function(char* path_name);
 
 /**
- * @brief Check whether the path name is in the hash table
- * @return The address of the hash entry for the path name
+ * @brief Check whether the path name is present in the hash table.
+ *
+ * This function searches the hash table to check if the given path name
+ * is already present, returning the corresponding hash entry if found.
+ *
+ * @param path_name The path name to look for in the hash table.
+ *
+ * @return The address of the hash entry for the path name, or NULL if not found.
  */
 hash_entry* hash_table_lookup(char* path_name);
 
 /**
- * @brief Insert the LRU entry into the hash table
- * @return No return value
+ * @brief Insert the LRU entry into the hash table.
+ *
+ * This function inserts a new entry into the hash table, associating
+ * it with a path name and a corresponding cache entry.
+ *
+ * @param hd The LRU entry to insert into the hash table.
+ *
+ * @return No return value.
  */
 void hash_table_insert(lru_entry* hd);
 
 /**
- * @brief Remove the LRU entry from the hash table
- * @return 0, if success
- *         non-zero, if fail
+ * @brief Remove an LRU entry from the hash table.
+ *
+ * This function removes the specified entry from the hash table,
+ * effectively removing it from the cache.
+ *
+ * @param hd The LRU entry to remove from the hash table.
+ *
+ * @return 0 if the removal is successful, non-zero if it fails.
  */
 int hash_table_remove(lru_entry* hd);
+
 #endif
