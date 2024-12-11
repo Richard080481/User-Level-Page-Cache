26e3b270e03203a9b1eee38328adb1e805fca405
Richard <yongxiang080481@gmail.com>
2024-12-11 19:39:18 +0800

    fix write wrong flags

---
+++ page-cache/src/app.c
@@ -0,0 +1,67 @@
+#include <string.h>
+#include <error.h>
+#include <upage.h>
+
+int main(int argc, char* argv[])
+{
+    init_page_cache();
+    int STRING_LENGTH = 13;
+    char* test_string = (char*)malloc(STRING_LENGTH + 1);
+    if (test_string == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < STRING_LENGTH; i++)
+    {
+        test_string[i] = '0' + (i % 10);
+    }
+    test_string[STRING_LENGTH] = '\0';
+
+    uFILE* file = uopen("test_output.txt", "w");
+    if (file == NULL)
+    {
+        printf("Failed to open file for writing.\n");
+        free(test_string);
+        return 1;
+    }
+
+    if(uwrite(test_string, sizeof(int), STRING_LENGTH, file) != (size_t)STRING_LENGTH)
+    {printf("ERROR: uwrite\n");}
+    uclose(file);
+    printf("String written to file successfully.\n");
+
+    char* read_string = (char *)malloc(STRING_LENGTH + 1);
+    if (read_string == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        free(test_string);
+        return 1;
+    }
+    file = uopen("test_output.txt", "r");
+    if (file == NULL)
+    {
+        printf("Failed to open file for reading.\n");
+        free(test_string);
+        free(read_string);
+        return 1;
+    }
+    uread(read_string, sizeof(int), STRING_LENGTH, file);
+    read_string[STRING_LENGTH] = '\0';
+    uclose(file);
+
+    if (strcmp(test_string, read_string) == 0)
+    {
+        printf("Read and write are correct.\n");
+    } else
+    {
+        printf("Mismatch in read and write.\n");
+    }
+
+    free(test_string);
+    free(read_string);
+
+    exit_page_cache();
+    return 0;
+}
--- page-cache/src/upage.c
+++ page-cache/src/upage.c
@@ -119,7 +119,7 @@ int uclose(uFILE* stream)
 
 size_t uwrite(const void* buffer, size_t size, size_t count, uFILE* stream)
 {
-    if (((stream->mode) & U_OREAD) == 0) // if U_OREAD is 0, this file cannot be written to; return 0
+    if (((stream->mode) & U_OWRITE) == 0) // if U_OREAD is 0, this file cannot be written to; return 0
     {
         return 0;
     }
@@ -274,65 +274,4 @@ size_t uread(void* buffer, size_t size, size_t count, uFILE* stream)
     }
 
     return write_cnt;
-}
-
-int main(int argc, char* argv[])
-{
-    init_page_cache();
-    int STRING_LENGTH = 13000;
-    char *test_string = (char *)malloc(STRING_LENGTH + 1);
-    if (test_string == NULL)
-    {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
-
-    for (int i = 0; i < STRING_LENGTH; i++)
-    {
-        test_string[i] = '0' + (i % 10);
-    }
-    test_string[STRING_LENGTH] = '\0';
-
-    uFILE *file = uopen("test_output.txt", "w");
-    if (file == NULL)
-    {
-        printf("Failed to open file for writing.\n");
-        free(test_string);
-        return 1;
-    }
-    uclose(file);
-    printf("String written to file successfully.\n");
-
-    char *read_string = (char *)malloc(STRING_LENGTH + 1);
-    if (read_string == NULL)
-    {
-        printf("Memory allocation failed.\n");
-        free(test_string);
-        return 1;
-    }
-    file = uopen("test_output.txt", "r");
-    if (file == NULL)
-    {
-        printf("Failed to open file for reading.\n");
-        free(test_string);
-        free(read_string);
-        return 1;
-    }
-    uread(read_string, 1, STRING_LENGTH, file);
-    read_string[STRING_LENGTH] = '\0';
-    uclose(file);
-
-    if (strcmp(test_string, read_string) == 0)
-    {
-        printf("Read and write are correct.\n");
-    } else
-    {
-        printf("Mismatch in read and write.\n");
-    }
-
-    free(test_string);
-    free(read_string);
-
-    exit_page_cache();
-    return 0;
 }
